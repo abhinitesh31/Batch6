3eee0001cd12cc4110083fc882a1d6318e9b2dda
codeclouds-abhinitesh <abhinitesh.93@gmail.com>
2024-05-03 06:59:00 +0530

    Push

--- src/basic/April11/LAB009_Fizz.java
+++ src/basic/April11/LAB009_Fizz.java
@@ -10,12 +10,14 @@ public class LAB009_Fizz {
                 System.out.println("FIZZ BUZZ is Printed for multiple of 5 and 3 both");
             } else if (i % 5 == 0) {
                 System.out.println("BUZZ is Printed for multiple of 5");
-            } else  if (i % 3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("FIZZ is Printed for multiple of 3");
             } else {
                 System.out.println(i);
             }
-        }}}
+        }
+    }
+}
 
 
 
